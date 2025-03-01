4847e24de64e905658136f8b5bc21e0e69ad6a5d
doniyor209 <doniyortoshev123@gmail.com>
2025-03-01 15:53:14 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+const GAMER = {
+    FirstName: null,
+    ModemID: null,
+    Password: null,
+}
+
+console.log(GAMER);
+
+GAMER.FirstName=prompt("Ismingizni Kiriting")
+GAMER.ModemID=prompt("Spacedagi idingizni kiriting")
+GAMER.Password=prompt("Paswordingizni kiriting")
+
+console.log(GAMER);
